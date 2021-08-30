2401c3999900233fa4a1270052b01252479bb751
Faisal L <salluthdev@gmail.com>
2021-08-30 10:40:04 +0700

    Penambahan Kerangka JS Lanjutan

--- assets/script.js
+++ assets/script.js
@@ -6,5 +6,9 @@ function element(totalPages, page){
 	if(page > 1){ //if page value is greater than 1 then add new li which is previous button
 		liTag = "<li class="btn-pagination prev"><span><i class="fas fa-angle-left"></i>Prev</span></li>";
 	}
+	if(page < totalPages){ //if page value is less than totalPages value then add new li which is next button
+		liTag = "<li class="btn-pagination prev"><span><i class="fas fa-angle-left"></i>Prev</span></li>";
+	}
+	ulTag.innerHTML = liTag;
 }
 element(20, 5); // calling above function with passing values
