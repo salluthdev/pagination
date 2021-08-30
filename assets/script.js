d1b64720c2dacd5aacb74d1d09b1b704ef769d0f
Faisal L <salluthdev@gmail.com>
2021-08-30 10:36:49 +0700

    Penambahan Kerangka JavaScript

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// selecting required element
+const ulTag = document.querySelector("ul");
+
+function element(totalPages, page){
+	let liTag = '';
+	if(page > 1){ //if page value is greater than 1 then add new li which is previous button
+		liTag = "<li class="btn-pagination prev"><span><i class="fas fa-angle-left"></i>Prev</span></li>";
+	}
+}
+element(20, 5); // calling above function with passing values
